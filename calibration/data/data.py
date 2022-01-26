7c0075107c4ea041a1d3c83d6afaa2412be16d10
Konstantin Malanchev <hombit@gmail.com>
2022-01-26 11:43:51 -0600

    Initial GAIA EDR3 matching

--- calibration/data/data.py
+++ calibration/data/data.py
@@ -2,12 +2,17 @@ from functools import lru_cache
 from importlib.resources import open_binary
 
 from astropy.io import ascii
+from astropy.table import Table
 
 from calibration import data
 
 
 @lru_cache(1)
-def narayan_etal2019_t1():
+def narayan_etal2019_t1() -> Table:
+    """Slightly modified Table 1 from Narayan et al. 2019
+
+    https://ui.adsabs.harvard.edu/abs/2019ApJS..241...20N/abstract
+    """
     with open_binary(data, 'WDphot_ILAPHv5_abmag.tex') as fh:
         table = ascii.read(fh, format='latex')
     return table
