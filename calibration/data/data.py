2ef3f7209875e3d0c5ed6f97ac419901fa8d2c60
Konstantin Malanchev <hombit@gmail.com>
2022-02-16 10:24:11 -0600

    Docs for passband

--- calibration/data/data.py
+++ calibration/data/data.py
@@ -28,7 +28,18 @@ def narayan_etal2019_t1() -> Table:
 
 @lru_cache
 def passband(name: str) -> Table:
-    """Passband transmission from Filter Profile Service name"""
+    """Passband transmission from Filter Profile Service name
+
+    Returns
+    -------
+    Table
+        A table with two columns: 'wavelength' in angstroms and 'transmission'
+        The `meta` attribute contains a dictionary with:
+            - 'simpson_integral': transmission integral in angstroms
+            - 'spline': function returns interpolated transmission value,
+                the argument is a dimensional wavelength
+            - 'spline_integral': transmission integral in angstroms
+    """
     name = name.replace('/', '_')
     with open_binary(filters, f'{name}.dat') as fh:
         table = ascii.read(
@@ -41,7 +52,6 @@ def passband(name: str) -> Table:
 
     table.meta['simpson_integral'] = simpson(x=table['wavelength'], y=table['transmission']) * units.angstrom
     spline = UnivariateSpline(x=table['wavelength'], y=table['transmission'], k=3, ext='zeros')
-    # Spline is a function of a dimensional wavelength
     table.meta['spline'] = lambda lmbd: spline(lmbd.to_value(units.angstrom))
     # Spline integral could be a bit different from simpson integral
     table.meta['spline_integral'] = (
