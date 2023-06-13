e6d1a712126445f73c4bb9eb61ad25fae56ad17e
Konstantin Malanchev <hombit@gmail.com>
2023-06-13 15:51:28 -0500

    update scripts

--- scripts/apply-photo-transformation.sh
+++ scripts/apply-photo-transformation.sh
@@ -2,13 +2,7 @@
 
 for BAND in {g,r,i,z,y}; do
   echo $BAND
-  train-photo-transform \
-    data/ps1_des-grizy.parquet \
-    --input-bands g r i z y \
-    --modeldir=models/phot-transformation \
-    --figdir=figures/phot-transformation/des-ps1 \
-    --fig-support-color=r,i \
-    --input-survey=PS1 \
-    --output-survey=DES \
-    --output-band=$BAND
+  apply-photo-transform \
+    models/phot-transformation/DES_${BAND}-PS1_g--r--i--z--y.onnx \
+    data/ps1_stars.parquet
 done
